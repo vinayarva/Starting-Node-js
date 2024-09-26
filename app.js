38079dd27a6844a06fbdc669f46fbb9cf9d355be
vinayarva <vinaykumararva13@gmail.com>
2024-09-26 18:11:43 +0530

    Simple form filling project using node JS-final version

--- app.js
+++ app.js
@@ -3,42 +3,48 @@ const fs = require("fs")
 
 const server =  http.createServer((req,res)=>{
 
-    const text = fs.readFile('./message.txt', 'utf8',()=>{});
-
     const url = req.url
-    const method =  req.method
+    const method = req.method
 
-    res.setHeader("content-type","text/html")
+    if(url === "/" && method ==="GET"){
 
-    res.write("<html>")
-    res.write("<head><title>Simple Form</title></head>")
-    res.write("<body>")
-    res.write(`<h3>${text}</h3>`)
-    res.write("<form action='/' method='POST'><input type='text' name='message'><button type='submit'>Submit</button></form>")
-    res.write("</body>")
-    res.write("</html>")
-   
+            fs.readFile('./message.txt', 'utf8', (err, data) => {
+                if (err) {
+                    console.error("Error reading file:", err);
+                    return;
+                }
+                res.setHeader("content-type","text/html")
+                res.write("<html>")
+                res.write("<head><title>Simple Form</title></head>")
+                res.write("<body>")
+                res.write(`<h3>${data}</h3>`)
+                res.write("<form action='/' method='POST'><input type='text' name='message'><button type='submit'>Submit</button></form>")
+                res.write("</body>")
+                res.write("</html>")
+                res.end()
+            });
+        
 
-        if(url === "/" && method === "POST"){
-            const body = []
-            req.on("data",(chuck)=>{
-                 body.push(chuck)
-            })
-            return  req.on("end",()=>{
-                    const parsedBody = Buffer.concat(body).toString();
-                     const message = parsedBody.split('=')[1];
-                     fs.writeFile("message.txt",message,()=>{
-                        res.statusCode = 302;
-                        return res.end();
-                     })
-                        
-                        
+            
+        }else if( url === "/" && method === "POST"){
+                const body = []
+                req.on("data",(chuck)=>{
+                    body.push(chuck)
                 })
+                req.on("end",()=>{
+                    const parsedBody = Buffer.concat(body).toString();
+                    const message = parsedBody.split('=')[1];
+                    fs.writeFile("message.txt",message,(err)=>{
+                        if(err){
+                            console.log("Error writeing the file:"+err)
+                        }
+                        res.statusCode = 302;
+                        res.setHeader('Location', '/');
+                        return res.end();
+                    }) 
+                    })
         }
-
-        res.end()
     
-
 })
 
 
